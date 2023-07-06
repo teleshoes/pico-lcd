eb784bce699561c9b7993ee4569f7cb13ee55b0b
Elliot Wolk <elliot.wolk@gmail.com>
2023-07-05 23:19:50 -0400

    main: connect to wifi using wifi-conf.txt SSID/password file

--- src/main.py
+++ src/main.py
@@ -16,4 +16,56 @@ if __name__=='__main__':
   lcdFont = LcdFont('font5x8.bin', lcd)
   lcdFont.setup()
 
-  lcdFont.markup('!size=5!!color=green!HELLO\nWORLD')
+  networks = []
+  with open("wifi-conf.txt", "r") as fh:
+    for line in fh:
+      idx = line.find("=")
+      if idx > 0:
+        ssid = line[:idx].strip()
+        password = line[idx+1:].strip()
+        networks.append([ssid, password])
+
+  connected = False
+  connectedSSID = None
+  while not connected:
+    for ssidPassword in networks:
+      ssid = ssidPassword[0]
+      password = ssidPassword[1]
+
+      wlan = network.WLAN(network.STA_IF)
+      wlan.active(True)
+      try:
+        wlan.connect(ssid, password)
+      except Exception as e:
+        print(str(e))
+
+      lcdFont.markup(""
+        + "!size=4!!color=green!" + "WAITING\n"
+        + "!size=4!!color=green!" + "FOR WIFI\n"
+        + "!size=4!!color=white!" + "--------\n"
+        + "!size=2!!color=blue!"  + ssid
+      )
+
+      max_wait = 10
+      while max_wait > 0:
+          if wlan.status() < 0 or wlan.status() >= 3:
+              break
+          max_wait -= 1
+          print('waiting for connection...')
+          time.sleep(1)
+
+      if wlan.status() == 3:
+        connected = True
+        connectedSSID = ssid
+        break
+
+  ipAddr = None
+  if not connected:
+      lcdFont.markup("!size=7!!color=red!" + "FAILED\nWIFI")
+      raise RuntimeError('network connection failed')
+
+  else:
+      print('connected')
+      status = wlan.ifconfig()
+      print( 'ip = ' + status[0] )
+      ipAddr = status[0]
