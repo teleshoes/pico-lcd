78b026ed699502941d0aa4a9e8302335a6affb7a
Elliot Wolk <elliot.wolk@gmail.com>
2024-03-16 15:13:13 -0400

    install: generate+install tzdata zonename csv files

--- install.pl
+++ install.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use File::Basename qw(dirname);
 
 my @files = qw(
   font5x8.bin
@@ -11,6 +12,13 @@ my @files = qw(
   mpy/lcd.mpy
 );
 
+#DS3231 rtc can only handle 2 centuries anyway
+my $TZ_START_YEAR = 1900;
+my $TZ_END_YEAR = 2100;
+
+my @TZ_ZONENAMES = qw(
+);
+
 sub main(@){
   if(not -f "font5x8.bin"){
     system "python", "src/font-generator.py";
@@ -18,6 +26,15 @@ sub main(@){
   if(not -f "font5x8.bin"){
     die "ERROR: could not generate font binary\n";
   }
+
+  #calculate+install tzdata CSV files
+  system "./zoneinfo-tool",
+    "-c", "$TZ_START_YEAR,$TZ_END_YEAR",
+    "--skip-existing",
+    @TZ_ZONENAMES,
+  ;
+  system "./rshell", "rsync", "--mirror", "./zoneinfo", "/pyboard/zoneinfo/";
+
 
   system "rm", "-rf", "mpy/";
   system "mkdir", "mpy";
